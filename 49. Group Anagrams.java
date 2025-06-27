37238b5718566086ef0d30702d686ce0652b8350
SahajDang <115211400+SahajDang@users.noreply.github.com>
2025-06-27 23:41:48 +0530

    Leetcode - 49. Group Anagrams.java

---
+++ 49. Group Anagrams.java
@@ -0,0 +1,56 @@
+class Solution {
+    // boolean isAnagram(String first, String second){
+    //     if(first.length() != second.length()){
+    //         return false;
+    //     }
+    //     char[] x = first.toCharArray();
+    //     char[] y = second.toCharArray();
+    //     Arrays.sort(x);
+    //     Arrays.sort(y);
+    //     return Arrays.equals(x, y);
+    // }
+    // TC - (n^2 n logn)
+    // Brute force Solution
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // List<List<String>> bigList = new ArrayList<>();
+        // for(String word : strs){
+        //     boolean added = false; //flag to check if it is already in the list
+        //     for(List<String> smallList : bigList){
+        //         if(isAnagram(smallList.get(0), word)){
+        //             smallList.add(word);
+        //             added = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!added){
+        //         List<String> newSmallList = new ArrayList<>();
+        //         newSmallList.add(word);
+        //         bigList.add(newSmallList);
+        //     }
+        // }
+        // return bigList;
+
+        // Map<String, List<String>> map = new HashMap<>();
+        // for(String word : strs){
+        //     char w[] = word.toCharArray();
+        //     Arrays.sort(w);
+        //     String key = new String(w);
+        //     map.computeIfAbsent(key, k->new ArrayList<>()).add(word);
+        // }
+        // return new ArrayList<>(map.values());
+        Map<String, List<String>> map = new HashMap<>();
+        for(String word : strs){
+            int countArr[] = new int[28];
+            for(char c : word.toCharArray()){
+                countArr[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int k : countArr){
+                sb.append(k).append('#');
+            }
+            String key = sb.toString();
+            map.computeIfAbsent(key, k->new ArrayList<>()).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
